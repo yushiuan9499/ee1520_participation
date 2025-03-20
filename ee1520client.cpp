b6a03f8253cd5b1859eda26464d93ec4cef3b524
yushiuan9499 <yushiuan9499@gmail.com>
2025-03-20 11:15:17 +0800

    save a record at local

--- ee1520client.cpp
+++ ee1520client.cpp
@@ -2,6 +2,7 @@
 // ecs36b first program
 
 #include "ee1520_Exception.h"
+#include <fstream>
 #include <iostream>
 
 // JSON RPC part
@@ -16,10 +17,21 @@ int main(int argc, char *argv[]) {
   // HttpClient httpclient("http://127.0.0.1:8384");
 
   if (argc != 2) {
-    cerr << "must provide a message" << endl;
+    cerr << "must provide exactly a message" << endl;
     return 1;
   }
   cout << "Sending msg: " << argv[1] << endl;
+  {
+    ifstream responseFile("response.json");
+    if (responseFile.good()) {
+      Json::Value prevResponse;
+      responseFile >> prevResponse;
+      if (prevResponse.isMember(argv[1])) {
+        cout << "Already participated in this survey" << endl;
+        return 0;
+      }
+    }
+  }
   // connecting to your instructor's laptop
   HttpClient httpclient("https://ethikos.ngrok.io");
   ee1520Client myClient(httpclient, JSONRPC_CLIENT_V2);
@@ -34,6 +46,10 @@ int main(int argc, char *argv[]) {
     cerr << e.what() << endl;
   }
 
+  if (myv["status"] != "failed") {
+    ofstream responseFile("response.json");
+    responseFile << myv.toStyledString();
+  }
   std::cout << myv.toStyledString() << std::endl;
   return 0;
 }
